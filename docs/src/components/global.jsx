488daaacf7d4e051afd13f2971a11eb375e50389
userdocs <16525024+userdocs@users.noreply.github.com>
2024-03-21 19:29:47 +0000

    docs

--- docs/src/components/global.jsx
+++ docs/src/components/global.jsx
@@ -2,7 +2,9 @@ import Advanced from "/src/components/advancedMarkdown.astro"
 import Charts from "/src/components/charts.astro"
 import Details from "/src/components/details.astro";
 import Modal from '/src/components/modal.astro';
+import BuildInfo from "/src/components/buildinfo.astro"
+import Changelog from "/src/components/changelog.astro"
 
 import { Steps, Tabs, TabItem, Card, CardGrid, LinkCard, Aside, Icon } from '@astrojs/starlight/components';
 
-export { Advanced, Charts, Details, Modal, Steps, Tabs, TabItem, Card, CardGrid, LinkCard, Aside, Icon };
+export { Advanced, BuildInfo, Changelog, Charts, Details, Modal, Steps, Tabs, TabItem, Card, CardGrid, LinkCard, Aside, Icon };
