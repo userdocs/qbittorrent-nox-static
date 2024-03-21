091ffb55bc7f0911c31bae689960c5d7987d0c11
userdocs <16525024+userdocs@users.noreply.github.com>
2024-03-21 19:33:42 +0000

    Update global.jsx

--- docs/src/components/global.jsx
+++ docs/src/components/global.jsx
@@ -3,8 +3,7 @@ import Charts from "/src/components/charts.astro"
 import Details from "/src/components/details.astro";
 import Modal from '/src/components/modal.astro';
 import BuildInfo from "/src/components/buildinfo.astro"
-import Changelog from "/src/components/changelog.astro"
 
 import { Steps, Tabs, TabItem, Card, CardGrid, LinkCard, Aside, Icon } from '@astrojs/starlight/components';
 
-export { Advanced, BuildInfo, Changelog, Charts, Details, Modal, Steps, Tabs, TabItem, Card, CardGrid, LinkCard, Aside, Icon };
+export { Advanced, BuildInfo, Charts, Details, Modal, Steps, Tabs, TabItem, Card, CardGrid, LinkCard, Aside, Icon };
