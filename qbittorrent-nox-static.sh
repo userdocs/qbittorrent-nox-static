646a6e5ca5f42f1c7c52a09177a82b3f83634cf0
userdocs <16525024+userdocs@users.noreply.github.com>
2024-12-05 08:50:21 +0000

    missing comma
    Update matrix_multi_build_and_release_qbt_workflow_files.yml
    
    workflows
    
    if libtorrent RC_1_2 is not updated to work with boost 1.87.0 i'll need to have unique versions of boost per release. this change anticipates that outcome.
    
    Update matrix_multi_build_and_release_qbt_workflow_files.yml

--- qbittorrent-nox-static.sh
+++ qbittorrent-nox-static.sh
@@ -492,7 +492,7 @@ _qbittorrent_build_cons() {
 _set_build_cons() {
 	if [[ $(_qbittorrent_build_cons) == "yes" && "${qbt_qt_version}" == "5" ]]; then
 		printf '\n%b\n\n' " ${text_blink}${unicode_red_light_circle}${color_end} ${color_yellow}qBittorrent ${color_magenta}${github_tag[qbittorrent]}${color_yellow} does not support ${color_red}Qt5${color_yellow}. Please use ${color_green}Qt6${color_yellow} or a qBittorrent ${color_green}v4${color_yellow} tag.${color_end}"
-		if [[ -d "${release_info_dir}" ]]; then touch "${release_info_dir}/disable-qt5"; fi # qbittorrent v5 transtion - workflow specific
+		if [[ -d "${release_info_dir}" ]]; then touch "${release_info_dir}/disable-qt5"; fi # qbittorrent v5 transition - workflow specific
 		exit                                                                                # non error exit to not upset github actions - just skip the step
 	fi
 }
